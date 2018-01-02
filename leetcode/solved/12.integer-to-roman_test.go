5f3c40873105320004fb7752e06675567aea8a55
soundlessthj <soundless.thj@gmail.com>
2018-01-02 00:34:56 -0800

    add test for 12th

---
+++ leetcode/solved/12.integer-to-roman_test.go
@@ -0,0 +1,27 @@
+package gogo
+
+import "testing"
+
+func Test_12_intToRoman(t *testing.T) {
+    tests := map[int]string {
+        1: "I",
+        8: "VIII",
+        10: "X",
+        23: "XXIII",
+        99: "XCIX",
+        123: "CXXIII",
+        545: "DXLV",
+        1000: "M",
+        1024: "MXXIV",
+        1999: "MCMXCIX",
+        2000: "MM",
+        3999: "MMMCMXCIX",
+    }
+
+    for key, value := range(tests) {
+        actual := intToRoman(key)
+        if actual != value {
+            t.Fatalf("test: %v, expected: %v, actual: %v", key, value, actual)
+        }
+    }
+}
