4a0f498c40044857f3732c316d805b7e1ea10080
soundlessthj <soundless.thj@gmail.com>
2018-01-08 00:09:34 -0800

    010818 commits

--- leetcode/solved/16.3sum-closest_test.go
+++ leetcode/solved/16.3sum-closest_test.go
@@ -10,4 +10,12 @@ func Test_16_3sum_closest(t *testing.T) {
     if expected != actual {
         t.Fatalf("tests: %v, target: %v, expected: %v, actual: %v", S, target, expected, actual)
     }
+
+    S1 := []int{-1, 0, 1, 2, -1, -4}
+    target1 := 4
+    expected1 := 3
+    actual1 := threeSumClosest(S1, target1)
+    if expected1 != actual1 {
+        t.Fatalf("tests: %v, target: %v, expected: %v, actual: %v", S1, target1, expected1, actual1)
+    }
 }
